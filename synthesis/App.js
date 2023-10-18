c39c5e902563c7401ae33aa4b504a434be1c7262
MajorDown <romain.fouillaron@gmx.fr>
2023-10-18 23:34:53 +0200

    echec de l'utilisation de expo-av, début de test avec RNSound

--- synthesis/App.js
+++ synthesis/App.js
@@ -1,13 +1,41 @@
-import { useState, useRef } from "react";
+import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import { Button, View } from "react-native";
 import { styles } from "./styles/styles";
 import { NotesDataProvider } from "./contexts/notesDataContext";
 import AxisController from "./components/AxisController";
 import ChannelBar from "./components/ChannelBar";
 
+import Sound from "react-native-sound";
+
 export default function App() {
   const [touches, setTouches] = useState([]);
+
+  Sound.setCategory("Playback");
+  let cornemuse = new Sound(
+    "/assets/sounds/cornemuse.mp3",
+    Sound.MAIN_BUNDLE,
+    (err) => {
+      if (err) {
+        console.log("failed to load sound :", err);
+        return;
+      }
+      console.log(
+        "duration in seconds: " +
+          cornemuse.getDuration() +
+          "number of channels: " +
+          cornemuse.getNumberOfChannels()
+      );
+      // Play the sound with an onEnd callback
+      cornemuse.play((success) => {
+        if (success) {
+          console.log("successfully finished playing");
+        } else {
+          console.log("playback failed due to audio decoding errors");
+        }
+      });
+    }
+  );
 
   return (
     <View style={styles.app}>
@@ -15,6 +43,7 @@ export default function App() {
       <NotesDataProvider>
         <AxisController firstNote={"C"} tessiture={2} onControl={setTouches} />
         <ChannelBar />
+        <Button title="sound" onPress={() => playSound()}></Button>
       </NotesDataProvider>
     </View>
   );
